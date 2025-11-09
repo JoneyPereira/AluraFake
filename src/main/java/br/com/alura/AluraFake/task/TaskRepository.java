721c554bd13edb98d51fceef72e2a3cdb3a02f60
Joney Sousa <joneysousa@yahoo.com.br>
2025-11-09 12:43:21 -0300

    feat: Adicionadas as validações no post de opentext.

--- src/main/java/br/com/alura/AluraFake/task/TaskRepository.java
+++ src/main/java/br/com/alura/AluraFake/task/TaskRepository.java
@@ -1,7 +1,11 @@
 package br.com.alura.AluraFake.task;
 
+import br.com.alura.AluraFake.course.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface TaskRepository extends JpaRepository<Task, Long>{
-
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    @Query("SELECT COUNT(t) > 0 FROM Task t WHERE t.course = :course AND t.statement = :statement")
+    boolean existsByStatementAndCourse(@Param("statement") String statement, @Param("course") Course course);
 }
